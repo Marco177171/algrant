7b268009273ba17806d36f59a5bb8b2a040d1ea1
Marco177171 <msebastiani93@gmail.com>
2024-10-14 16:07:39 +0200

    Admin updated with table view

--- django_app/algrantapp/admin.py
+++ django_app/algrantapp/admin.py
@@ -3,8 +3,23 @@ from .models import Post, Comment, Friendship, Conversation, Message
 
 # Register your models here.
 
-admin.site.register(Post)
-admin.site.register(Comment)
-admin.site.register(Friendship)
+class PostAdmin(admin.ModelAdmin):
+    model = Post
+    list_display = ('created_by', 'content', 'date_time')
+    # list_editable = ('created_by', 'content', 'date_time') 
+admin.site.register(Post, PostAdmin)
+
+class CommentAdmin(admin.ModelAdmin):
+    model = Comment
+    list_display = ('created_by', 'content', 'post', 'date_time')
+    # list_editable = ('created_by', 'content', 'post', 'date_time')
+admin.site.register(Comment, CommentAdmin)
+
+class FriendshipAdmin(admin.ModelAdmin):
+    model = Friendship
+    list_display = ('from_user_id', 'to_user_id', 'is_active', 'seen')
+    # list_editable = ('from_user_id', 'to_user_id', 'is_active', 'seen')
+admin.site.register(Friendship, FriendshipAdmin)
+
 admin.site.register(Conversation)
 admin.site.register(Message)
