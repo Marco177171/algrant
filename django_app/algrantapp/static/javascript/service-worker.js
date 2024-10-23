cad40e3c95793304542ff5e89f08c43813585f75
Marco177171 <msebastiani93@gmail.com>
2024-10-23 14:33:47 +0200

    update

--- django_app/algrantapp/static/javascript/service-worker.js
+++ django_app/algrantapp/static/javascript/service-worker.js
@@ -1,11 +1,11 @@
 self.addEventListener('push', function(event) {
-    console.log('in SERVICE WORKER');
+    console.log('in SERVICE WORKER')
     const data = event.data.json();
     const options = {
-        title: body.title,
         body: data.body,
         icon: data.icon,
     };
+    console.log('OPTIONS: ' + options);
     event.waitUntil(
         self.registration.showNotification(data.title, options)
     );
@@ -13,7 +13,7 @@ self.addEventListener('push', function(event) {
 
 self.addEventListener('notificationclick', function(event) {
     // Define a URL to open when the notification is clicked
-    const targetUrl = '/my_conversations';  // Adjust based on your app's structure
+    const targetUrl = '/messages';  // Adjust based on your app's structure
     event.notification.close(); // Close the notification
 
     // Focus on the existing window if it's already open, or open a new one
