a2a8671a4a0317cd7b9d7290e81711bfd7d0557f
Marco177171 <msebastiani93@gmail.com>
2024-10-25 23:26:50 +0200

    SPONSORS! Models created, visualization in index.html managed via template_tags and custom_filters

--- django_app/algrantapp/templatetags/custom_tags.py
+++ django_app/algrantapp/templatetags/custom_tags.py
@@ -5,8 +5,8 @@ register = template.Library()
 @register.simple_tag(takes_context=True)
 def increase_index(context):
     index = context.get('index', 0)
-    if index < len(context['sponsors']):
-        context['index'] = index + 1
-    else:
+    if index >= len(context['sponsors']) - 1:
         context['index'] = 0
+    else:
+        context['index'] = index + 1
     return context['index']
